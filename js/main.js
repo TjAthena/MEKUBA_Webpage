04d19cc26379e32a57dbf1d2b0861a9aca30fc6d
Tjpradeep <107289568+TjAthena@users.noreply.github.com>
2024-10-18 18:22:54 +0530

    mobile

--- js/main.js
+++ js/main.js
@@ -90,34 +90,7 @@
 	});
 
 	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
-    autoplay: true,
-    stagePadding: 7,
-    margin: 20,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    nav: true,
-    autoplayHoverPause: true,
-    items: 4,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
-      },
-      600:{
-        items:3,
-        nav:false
-      },
-      1000:{
-        items:4,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+
 
 
 	var contentWayPoint = function() {
